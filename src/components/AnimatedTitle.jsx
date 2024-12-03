b384330d5f572ffd98b1fe445daa56a84e77a6e6
George Alan Rufo <georgealanrufo@gmail.com>
2024-12-02 22:54:20 -0300

    Continue in chapter animated title

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,11 @@
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+      Disc<b>o</b>ver the world&apos;s <br /> l<b>a</b>rgest shared adventure
+    </div>
+  ) 
+}
+
+export default AnimatedTitle;
+
+// TODO Continue in Animated Title, 01:10:00
