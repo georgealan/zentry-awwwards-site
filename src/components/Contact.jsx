961d658fd3f7fbb7329c806c282dc0b79f2dea2a
George Alan Rufo <georgealanrufo@gmail.com>
2024-12-04 12:53:36 -0300

    Continue in chapter contact and footer

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <main className="">
+   
+    </main>
+  ) 
+}
+
+export default Contact;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <main className="">
+   
+    </main>
+  ) 
+}
+
+export default Footer;
