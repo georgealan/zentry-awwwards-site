1faf7246eda38fe3430fec3587cf07b5880c26fa
George Alan Rufo <georgealanrufo@gmail.com>
2024-12-03 23:08:54 -0300

    Continue in chapter features

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,11 +1,49 @@
-const AnimatedTitle = () => {
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
+/* eslint-disable react/prop-types */
+const AnimatedTitle = ({ title, containerClass }) => {
+const containerRef = useRef(null);
+
+useEffect(() => {
+  const context = gsap.context(() => {
+    const titleAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: '100 bottom',
+        end: 'center bottom',
+        toggleActions: 'play none none reverse',
+      }
+    });
+
+    titleAnimation.to('.animated-word', {
+      opacity: 1,
+      transform: 'translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)',
+      stagger: 0.02,
+      ease: 'power2.inOut',
+    })
+  }, containerRef);
+
+  return () => context.revert();
+}, [])
+
   return (
-    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-      Disc<b>o</b>ver the world&apos;s <br /> l<b>a</b>rgest shared adventure
+    <div ref={containerRef} className={`animated-title ${containerClass}`}>
+      {title.split('<br />').map((line, index) => (
+
+        <div key={index} className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3">
+
+        {line.split(' ').map((word, index) => (
+          <span key={index} className="animated-word" 
+          dangerouslySetInnerHTML={{ __html: word }} />
+        ))}
+
+        </div>
+        
+      ))}
     </div>
   ) 
 }
 
 export default AnimatedTitle;
 
-// TODO Continue in Animated Title, 01:10:00
---
+++ src/components/Features.jsx
@@ -0,0 +1,11 @@
+const Features = () => {
+  return (
+    <div className="">
+      
+    </div>
+  ) 
+}
+
+export default Features;
+
+// TODO continue in chapter features 01:41:00
